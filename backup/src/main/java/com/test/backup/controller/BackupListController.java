0951ad2adaebcbc74f6f968ce90c2b5124174cd4
Bulyha <43118118+Bulyha@users.noreply.github.com>
2018-10-31 00:05:00 +0200

    Update BackupListController.java

--- backup/src/main/java/com/test/backup/controller/BackupListController.java
+++ backup/src/main/java/com/test/backup/controller/BackupListController.java
@@ -2,21 +2,31 @@ package com.test.backup.controller;
 
 import com.test.backup.BackUp;
 import com.test.backup.repos.BackUpRepo;
-import com.test.backup.repos.ToDoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class BackupListController {
     @Autowired
-    private ToDoRepo toDoRepo;
-    @Autowired
     private BackUpRepo backUpRepo;
 
-    // This method return the list of all backups
+    // This method return the list of backups
     @GetMapping(path = "/list")
-    public Iterable<BackUp> backupList() {
-        return backUpRepo.findAll();
+    public String backupList(
+            Map<String, Object> model) {
+
+        List<BackUp> backUps = backUpRepo.findAll();
+
+        for (BackUp i: backUps) {
+            model.put("Backup '"+String.valueOf(i.getBackUpId())+"'", i.getBackUpId());
+            model.put("Date '"+String.valueOf(i.getDate())+"'", i.getDate());
+            model.put("Status '"+String.valueOf(i.getStatus())+"'", i.getStatus());
+        }
+        return "'"+model+"'";
     }
 }
+
